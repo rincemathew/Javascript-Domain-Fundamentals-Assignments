3facc6ca72d9ebac64e012a31ddc9153fe66de0b
Rince Mathew <rincemathewmanattu@gmail.com>
2023-02-27 12:31:42 +0530

    one added

--- 1.js
+++ 1.js
@@ -1,13 +1,11 @@
 // Accept a char input from the user and display it on the console.
 
+var read = require('readline-sync');
 
-function returnBinaryLetter(char) {
-    if ((/^[a-z]$/).test(char)) {
-      return char.charCodeAt(0).toString(2).padStart(8, '0')
-    } else {
-      return 'Sorry, that is not a letter.'
-    }
-  }
-  
-  var input = prompt('Enter letter to be converted to binary:').toLowerCase();
-  console.log(returnBinaryLetter(input))
+var character = read.question('type a char  ');
+
+if (character.length == 1) {
+  console.log(character)
+} else {
+  console.log('not a character')
+}
