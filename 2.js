9e73b7dbe350a0bc2c472a6bee631dd34a4f9b89
Rince Mathew <rincemathewmanattu@gmail.com>
2023-02-27 14:43:31 +0530

    number of added

--- 2.js
+++ 2.js
@@ -1,1 +1,9 @@
 // Accept two inputs from the user and output their sum.
+
+
+var read = require('readline-sync')
+
+var firstInput = read.question("type two numbers ")
+var secondInput = read.question()
+
+console.log(firstInput+secondInput)
